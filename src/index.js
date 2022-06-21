937b76cc1689632de1ab6bbde0154299905444d2
Dylan Palmboom <nofxboy1234@gmail.com>
2022-06-21 22:55:02 +0200

    Complete markup exercise

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import logo from './react-logo.png';
 // import './index.css';
 // import App from './App';
 // import reportWebVitals from './reportWebVitals';
@@ -13,14 +14,15 @@ import ReactDOM from 'react-dom/client';
 
 const page = (
   <div>
-    <h1>My awesome website in React</h1>
-    <h3>Reasons I love React</h3>
-    <ol>
-      <li>It's composable</li>
-      <li>It's declarative</li>
-      <li>It's a hireable skill</li>
-      <li>It's actively maintained by skilled people</li>
-    </ol>
+    <img src={logo} alt="react logo" width={40} />
+    <h1>Fun facts about React</h1>
+    <ul>
+      <li>Was first released in 2013</li>
+      <li>Was originally created by Jordan Walke</li>
+      <li>Has well over 100K stars on Github</li>
+      <li>Is maintained by Facebook</li>
+      <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ul>
   </div>
 );
 
