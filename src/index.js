949097028d315653020dfdfa3618880669fe99eb
Dylan Palmboom <nofxboy1234@gmail.com>
2022-06-23 18:42:46 +0200

    Create Header component

--- src/index.js
+++ src/index.js
@@ -11,14 +11,20 @@ import ReactDOM from 'react-dom/client';
 //   </React.StrictMode>
 // );
 
+function Header() {
+  return (
+    <header>
+      <nav>
+        <img src="./react-logo.png" alt="react logo" width="40px" />
+      </nav>
+    </header>
+  );
+}
+
 function Page() {
   return (
     <div>
-      <header>
-        <nav>
-          <img src="./react-logo.png" alt="react logo" width="40px" />
-        </nav>
-      </header>
+      <Header />
       <h1>Reasons I'm excited to learn React</h1>
       <ol>
         <li>Fun</li>
