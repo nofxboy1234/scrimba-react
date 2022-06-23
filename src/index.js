f47e743fce2cc71c361538085bc8a6414f2d5ee1
Dylan Palmboom <nofxboy1234@gmail.com>
2022-06-23 18:23:48 +0200

    Create a Page component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import logo from './react-logo.png';
 // import './index.css';
 // import App from './App';
 // import reportWebVitals from './reportWebVitals';
@@ -12,22 +11,28 @@ import logo from './react-logo.png';
 //   </React.StrictMode>
 // );
 
-const page = (
-  <div>
-    <img src={logo} alt="react logo" width={40} />
-    <h1>Fun facts about React</h1>
-    <ul>
-      <li>Was first released in 2013</li>
-      <li>Was originally created by Jordan Walke</li>
-      <li>Has well over 100K stars on Github</li>
-      <li>Is maintained by Facebook</li>
-      <li>Powers thousands of enterprise apps, including mobile apps</li>
-    </ul>
-  </div>
-);
+function Page() {
+  return (
+    <div>
+      <header>
+        <nav>
+          <img src="./react-logo.png" alt="react logo" width="40px" />
+        </nav>
+      </header>
+      <h1>Reasons I'm excited to learn React</h1>
+      <ol>
+        <li>Fun</li>
+        <li>Exciting</li>
+      </ol>
+      <footer>
+        <small>Copyright 2022 Palmboom development. All rights reserved.</small>
+      </footer>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(page);
+root.render(<Page />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
